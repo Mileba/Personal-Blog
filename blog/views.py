a43f3e2dd2f42ea30233a40ed2a9d57561c138e1
Mileba <mykellforbes@gmail.com>
2022-06-17 14:05:57 +0100

    updated models

--- blog/views.py
+++ blog/views.py
@@ -3,64 +3,6 @@ from datetime import date
 from .models import Post
 
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpeg",
-        "author": "Mileba",
-        "date": date(2022, 5, 1),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views in the mountain",
-        "content": '''
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Omnis dolore consequatur porro sit eius cum provident qui suscipit sed, voluptatum enim quod et qui? Ad nisi tempora vel blanditiis, tenetur laboriosam eum odit assumenda quam iure. Et eum illo sint commodi perspiciatis cumque reiciendis error veritatis quas aut, id nesciunt magni sequi distinctio, vero nostrum sequi voluptatum quo magnam maiores vitae eaque assumenda, facilis nam ab unde tempora repellat illo excepturi laborum?
-        '''
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpeg",
-        "author": "Mileba",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpeg",
-        "author": "Mileba",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-
-]
-
-
 # def get_date(Post):
 #     return Post.get('date')
 
